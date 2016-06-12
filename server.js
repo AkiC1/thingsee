827fecc09790c1c51534fc9d4ef4e88a44978059
AkiC1 <aki.piisinen@cloud1.fi>
2016-06-12 15:16:10 +0300

    Update server.js

--- server.js
+++ server.js
@@ -1,5 +1,8 @@
 var http = require('http')
 var port = process.env.PORT || 1337;
+var EventHub = require('eventhub_proxy.js ');
+var app = new EventHub();
+
 http.createServer(function(req, res) {
   res.writeHead(200, { 'Content-Type': 'text/plain' });
   res.end('Hello World\n');
